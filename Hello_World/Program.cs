0722c6df299681d3240a97a39eaae1c09d1cba34
abernhard <andy.bernhard@gmx.ch>
2020-03-20 15:55:22 +0100

    Add Menue Options

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -11,6 +11,20 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Welt 2020!");
+
+            var lunchLoader = new LunchLoader();
+            Console.WriteLine("Press v for Vegi, and key for Meat");
+            Console.WriteLine();
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.V)
+            {
+
+                lunchLoader.PrintVegi();
+            }
+            else
+            {
+                lunchLoader.PrintMeat();
+            }
             Console.Read();
         }
     }
