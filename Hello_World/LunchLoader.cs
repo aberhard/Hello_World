f6a985e5f5ef228970acc7771326b66f37563ec1
abernhard <andy.bernhard@gmx.ch>
2020-03-25 08:30:31 +0100

    Dateien geändert

--- Hello_World/LunchLoader.cs
+++ Hello_World/LunchLoader.cs
@@ -1,22 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello_World
 {
-    class LunchLoader
+     public class LunchLoader
     {
-        internal void PrintVegi()
-        {
-            Console.WriteLine("Kartoffeln");
-        }
-
 
         internal void PrintMeat()
         {
-            Console.WriteLine("SchnitzelKartoffeln");
+            Console.WriteLine("Schnitzel");
+
+        }
+
+
+
+
+
+        internal void PrintVegi()
+        {
+            Console.WriteLine("Kartoffeln");
+
         }
     }
 }
--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -11,7 +11,25 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Welt 2020!");
+           
+         
+            Console.WriteLine("Press v for Vegi, and key for Meat");
+            Console.WriteLine();
+            var key = Console.ReadKey();
+            if (key.Key == ConsoleKey.V)
+            {
+
+               PrintVegi();
+            }
+            else
+            {
+                PrintMeat();
+            }
             Console.Read();
+
+            
+
+
         }
     }
 }
